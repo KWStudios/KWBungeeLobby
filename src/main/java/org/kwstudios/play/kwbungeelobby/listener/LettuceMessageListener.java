39cad194bb35ba70c89a661f27a34ef4f128b725
Ybrin <koray@koska.at>
2016-01-31 17:53:53 +0100

    Make lettuce asynchronous

--- src/main/java/org/kwstudios/play/kwbungeelobby/listener/LettuceMessageListener.java
+++ src/main/java/org/kwstudios/play/kwbungeelobby/listener/LettuceMessageListener.java
@@ -18,52 +18,58 @@ public abstract class LettuceMessageListener {
 	public abstract void taskOnMessageReceive(String channel, String message);
 
 	private void setupListener() {
-		RedisPubSubConnection<String, String> connection = PluginLoader.getRedisClient().connectPubSub();
-		connection.addListener(new RedisPubSubListener<String, String>() {
+		Bukkit.getServer().getScheduler().runTaskAsynchronously(PluginLoader.getInstance(), new Runnable() {
 
 			@Override
-			public void unsubscribed(String channel, long count) {
-				// TODO Auto-generated method stub
-				Bukkit.getConsoleSender().sendMessage("Unsubscribed from the channel: " + channel);
-			}
+			public void run() {
+				RedisPubSubConnection<String, String> connection = PluginLoader.getRedisClient().connectPubSub();
+				connection.addListener(new RedisPubSubListener<String, String>() {
 
-			@Override
-			public void subscribed(String channel, long count) {
-				// TODO Auto-generated method stub
-				Bukkit.getConsoleSender().sendMessage("Subscribed to the channel: " + channel);
-			}
+					@Override
+					public void unsubscribed(String channel, long count) {
+						// TODO Auto-generated method stub
+						Bukkit.getConsoleSender().sendMessage("Unsubscribed from the channel: " + channel);
+					}
 
-			@Override
-			public void punsubscribed(String pattern, long count) {
-				// TODO Auto-generated method stub
+					@Override
+					public void subscribed(String channel, long count) {
+						// TODO Auto-generated method stub
+						Bukkit.getConsoleSender().sendMessage("Subscribed to the channel: " + channel);
+					}
 
-			}
+					@Override
+					public void punsubscribed(String pattern, long count) {
+						// TODO Auto-generated method stub
 
-			@Override
-			public void psubscribed(String pattern, long count) {
-				// TODO Auto-generated method stub
+					}
 
-			}
+					@Override
+					public void psubscribed(String pattern, long count) {
+						// TODO Auto-generated method stub
 
-			@Override
-			public void message(String pattern, String channel, String message) {
-				// TODO Auto-generated method stub
-				Bukkit.getConsoleSender().sendMessage("Received a new message with the pattern " + pattern);
-			}
+					}
 
-			@Override
-			public void message(String channel, String message) {
-				// TODO Auto-generated method stub
-				Bukkit.getConsoleSender().sendMessage("Lettuce received a new message from the Redis Host!");
-				Bukkit.getConsoleSender().sendMessage(channel);
-				Bukkit.getConsoleSender().sendMessage(message);
-				taskOnMessageReceive(channel, message);
+					@Override
+					public void message(String pattern, String channel, String message) {
+						// TODO Auto-generated method stub
+						Bukkit.getConsoleSender().sendMessage("Received a new message with the pattern " + pattern);
+					}
 
+					@Override
+					public void message(String channel, String message) {
+						// TODO Auto-generated method stub
+						Bukkit.getConsoleSender().sendMessage("Lettuce received a new message from the Redis Host!");
+						Bukkit.getConsoleSender().sendMessage(channel);
+						Bukkit.getConsoleSender().sendMessage(message);
+						taskOnMessageReceive(channel, message);
+
+					}
+				});
+
+				Bukkit.getConsoleSender().sendMessage("Subscribing to the given channels!");
+				connection.subscribe(channels);
 			}
 		});
-
-		Bukkit.getConsoleSender().sendMessage("Subscribing to the given channels!");
-		connection.subscribe(channels);
 	}
 
 }
